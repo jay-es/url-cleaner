88b192602f89ab3163b6a1af850f379e9dbae959
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-07-04 16:33:24 +0900

    2way-binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   const searchParams = new URLSearchParams(window.location.search)
-  const url = searchParams.get('text') ?? ''
+  let url = searchParams.get('text') ?? ''
 
   $: href = url.replace(/[?#].*/, "")
 </script>
 
 <main>
   <h1>URL Cleaner</h1>
-  <textarea value={url}></textarea>
+  <textarea bind:value={url}></textarea>
   <a {href} target="_blank">{href}</a>
 </main>
 
